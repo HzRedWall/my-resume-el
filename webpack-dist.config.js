5bfcf345a10e57ec506008ad98c6de2cc376da77
Meng Wang <wmlucas@163.com>
2018-09-29 11:17:03 +0800

    fix  bugs

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -96,7 +96,7 @@ module.exports = {
     }),
     new EndWebpackPlugin(async () => {
       // 自定义域名
-      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'resume.wuhaolin.cn');
+      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), '127.0.0.1');
 
       await publishGhPages();
 
